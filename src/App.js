afc5b3fef838076ae7f72ddd898d928e92644c16
1Sayd <person.tak@gmail.com>
2023-01-22 17:20:06 +0200

    Last testing of commitnig

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          For test commit
-        </p>
-        <p>
-          For test [ commit ]
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
