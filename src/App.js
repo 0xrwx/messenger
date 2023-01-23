4543b938f2a066c2590d810b4c94b2072df2ad01
1Sayd <person.tak@gmail.com>
2023-01-23 13:44:57 +0200

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import './input.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-Header">
-        <h1>Hello messenger</h1>
+        <h1 className={"text-7xl font-bold underline"}>Hello messenger</h1>
       </header>
       <main className="App-Main">
 
