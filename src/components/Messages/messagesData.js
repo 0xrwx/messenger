742b3e068ace11344ae1d035e7f5f918cb667a34
1Sayd <person.tak@gmail.com>
2023-01-27 15:54:25 +0200

    Component for rendering Messages and template data

---
+++ src/components/Messages/messagesData.js
@@ -0,0 +1,43 @@
+export const messagesData = [
+  {
+    person_id: 2,
+    text: 'Yo Hello everyone!!',
+  },
+  {
+    person_id: 2,
+    text: 'How was yours day?',
+  },
+  {
+    person_id: 4,
+    text: 'Thanks for asking, everything good 😊',
+  },
+  {
+    person_id: 0,
+    text: 'Zapayaniy 💀💀💀',
+  },
+  {
+    person_id: 2,
+    text: '🙁',
+  },
+  {
+    person_id: 1,
+    text: 'My day also zapayanity 😒😒😒',
+  },
+  {
+    person_id: 2,
+    text: 'Ehh...',
+  },{
+    person_id: 2,
+    text: 'Wish next day will be better 💡',
+  },{
+    person_id: 0,
+    text: '🥠🥠🥠',
+  },{
+    person_id: 1,
+    text: '🥠🥠🥠',
+  },
+  {
+    person_id: 3,
+    text: '🥠🥠🥠',
+  },
+]
